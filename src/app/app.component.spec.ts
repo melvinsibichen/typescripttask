464e5fca65376c7247c1eb01562dac5949dbf35e
Melvin Sibichen <melvinsibichen1020@gmail.com>
2023-11-13 19:07:18 +0530

    var let const examples

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'typescripttask'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('typescripttask');
+    // expect(app.title).toEqual('typescripttask');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typescripttask';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {  
+    this.example(); 
+  }
+
+  example(){
+    var variable1='Hello';
+    var variable2='world';
+    var variable3='welcome';
+
+    if(2>1){
+      var variable1='goodbye';
+      let variable2='universe';
+      console.log('1',variable1)
+      console.log('2',variable2)
+      console.log('3',variable3)
+    }
+    console.log('4',variable1)
+      console.log('5',variable2)
+      console.log('6',variable3)
+  }
+ 
+
+
 }
+
+// var example1
+
+// var i
+//   for( i=0;i<2;)
+//   {
+//     console.log(i)
+//     i++;
+//   }
+
+
+
+// var example2
+
+  // for(var i=0;i<2;i++){
+
+  // }
+  // console.log(i)
+
+
+  // let example1
+
+
+  // for(let i=0;i<2;i++){
+  //   console.log(i)
+  // }
+  
+
+   // const example1
+  //  for (const i=0; i<2; i++){
+  //   console.log(i)
+  //  }
